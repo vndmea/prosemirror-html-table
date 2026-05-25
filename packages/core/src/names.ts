e8098818208107ba73ff0fec59be6cfd9157a48f
vndmea <vndmea@gmail.com>
2026-05-25 22:38:36 +0800

    feat(core): add default node names

---
+++ packages/core/src/names.ts
@@ -0,0 +1,16 @@
+export const htmlTableNodeNames = {
+  table: 'htmlTable',
+  caption: 'htmlTableCaption',
+  colgroup: 'htmlTableColgroup',
+  col: 'htmlTableCol',
+  head: 'htmlTableHead',
+  body: 'htmlTableBody',
+  foot: 'htmlTableFoot',
+  row: 'htmlTableRow',
+  headerCell: 'htmlTableHeaderCell',
+  cell: 'htmlTableCell',
+} as const;
+
+export type HtmlTableNodeNameKey = keyof typeof htmlTableNodeNames;
+
+export type HtmlTableNodeNames = typeof htmlTableNodeNames;
