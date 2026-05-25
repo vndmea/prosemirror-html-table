1b66c05cbd19777242787b91816f9614ff68fc09
vndmea <vndmea@gmail.com>
2026-05-25 22:41:05 +0800

    feat(tiptap): add shared extension options

---
+++ packages/tiptap/src/options.ts
@@ -0,0 +1,7 @@
+export interface HtmlTableTiptapOptions {
+  HTMLAttributes: Record<string, unknown>;
+}
+
+export const defaultHtmlTableTiptapOptions: HtmlTableTiptapOptions = {
+  HTMLAttributes: {},
+};
