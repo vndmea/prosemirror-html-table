1abaf4f8ae24f0e5d15beb4e158ae1d36944b64c
vndmea <vndmea@gmail.com>
2026-05-25 22:53:22 +0800

    feat(core): export insert table command

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,9 +1,11 @@
 export { createHtmlTableNode } from './builders.js';
+export { insertHtmlTable } from './commands.js';
 export { htmlTableNodeNames } from './names.js';
 export { createHtmlTableGrid, getCellAt, isCellAnchor } from './grid.js';
 export { createHtmlTableNodeSpecs, normalizeHtmlTableSchemaOptions } from './schema.js';
 
 export type { CreateHtmlTableOptions } from './builders.js';
+export type { InsertHtmlTableCommandOptions } from './commands.js';
 
 export type {
   HtmlTableNodeNameKey,
