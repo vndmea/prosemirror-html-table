ddf11f75c1d2a1d0af1840b27412ce4238c06dde
vndmea <vndmea@gmail.com>
2026-05-25 22:54:55 +0800

    feat(tiptap): export command helpers

--- packages/tiptap/src/index.ts
+++ packages/tiptap/src/index.ts
@@ -1,3 +1,5 @@
+export { createHtmlTableCommands } from './commands.js';
+
 export {
   HtmlTable,
   HtmlTableBody,
@@ -13,4 +15,5 @@ export {
 } from './nodes.js';
 
 export { defaultHtmlTableTiptapOptions } from './options.js';
+export type { InsertHtmlTableCommandOptions } from './commands.js';
 export type { HtmlTableTiptapOptions } from './options.js';
