0dcd1a923b2bd1292358e88c261c6318da5a107b
vndmea <vndmea@gmail.com>
2026-05-25 22:41:45 +0800

    feat(tiptap): add public exports

---
+++ packages/tiptap/src/index.ts
@@ -0,0 +1,16 @@
+export {
+  HtmlTable,
+  HtmlTableBody,
+  HtmlTableCaption,
+  HtmlTableCell,
+  HtmlTableCol,
+  HtmlTableColgroup,
+  HtmlTableExtensions,
+  HtmlTableFoot,
+  HtmlTableHead,
+  HtmlTableHeaderCell,
+  HtmlTableRow,
+} from './nodes.js';
+
+export { defaultHtmlTableTiptapOptions } from './options.js';
+export type { HtmlTableTiptapOptions } from './options.js';
