191b9817efb29b6d05beb7d13602c251bc1b0fca
vndmea <vndmea@gmail.com>
2026-05-25 22:39:47 +0800

    feat(core): add public exports

---
+++ packages/core/src/index.ts
@@ -0,0 +1,13 @@
+export { htmlTableNodeNames } from './names.js';
+export { createHtmlTableNodeSpecs, normalizeHtmlTableSchemaOptions } from './schema.js';
+
+export type {
+  HtmlTableNodeNameKey,
+  HtmlTableNodeNames,
+} from './names.js';
+
+export type {
+  HtmlTableNodeSpecs,
+  HtmlTableSchemaOptions,
+  NormalizedHtmlTableSchemaOptions,
+} from './types.js';
