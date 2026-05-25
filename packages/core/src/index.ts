88f220612f3c9d3a19d67b654a828a9d6eda9b76
vndmea <vndmea@gmail.com>
2026-05-25 22:50:34 +0800

    feat(core): export table grid helpers

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,10 +1,20 @@
 export { htmlTableNodeNames } from './names.js';
+export { createHtmlTableGrid, getCellAt, isCellAnchor } from './grid.js';
 export { createHtmlTableNodeSpecs, normalizeHtmlTableSchemaOptions } from './schema.js';
 
 export type {
   HtmlTableNodeNameKey,
   HtmlTableNodeNames,
 } from './names.js';
+
+export type {
+  HtmlTableCellRef,
+  HtmlTableGrid,
+  HtmlTableGridOptions,
+  HtmlTableGridSlot,
+  HtmlTableRowRef,
+  HtmlTableSectionName,
+} from './grid.js';
 
 export type {
   HtmlTableNodeSpecs,
